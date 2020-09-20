ee9a2623a90a022b934a8526e2043bf4a963e740
Phillip Strom <phillip.strom@gmail.com>
2020-09-20 16:41:02 -0600

    Remove pry and comments

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,8 +1,7 @@
 class ForecastFacade
   def self.forecast_by_location(location)
-    require "pry"; binding.pry
     geocode = GeocodeService.search(location)
     forecast_data = ForecastService.forecast_by_coordinates(geocode)
-    Forecast.new(forecast_data, location) #location if necessary
+    Forecast.new(forecast_data, location)
   end
 end
