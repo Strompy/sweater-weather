63156a7b6aa3fafc933f25fdc17be32c88570c6c
Phillip Strom <phillip.strom@gmail.com>
2020-09-20 17:47:07 -0600

    Create bacakground image endpoint spec

---
+++ spec/requests/api/v1/forecasts/retrieve_image_for_city.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Get background image endpoint" do
+  it "returns image and data" do
+    location = 'denver,co'
+
+    get "/api/v1/backgrounds?location=#{location}"
+
+    expect(response).to be_successful
+    expect(response.content_type).to eq('application/json')
+    parsed = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    # assertions
+    expect(parsed[:type]).to eq('image')
+    image_info = parsed[:image]
+    expect(image_info.key).to eq([:location, :image_url, :credit])
+    expect(image_info[:location]).to eq(location)
+    expect(image_info[:image_url]).to_not be_nil
+    expect(image_info[:credit].keys).to eq([:source, :author, :logo])
+    expect(image_info[:credit].values).to_not include(nil)
+
+
+# {
+#   "type": "image",
+#   "id": null,
+#   "image": {
+#     "location": "denver,co",
+#     "image_url": "https://pixabay.com/get/54e6d4444f50a814f1dc8460962930761c38d6ed534c704c7c2878dd954dc451_640.jpg",
+#     "credit": {
+#       "source": "pixabay.com",
+#       "author": "quinntheislander",
+#       "logo": "https://pixabay.com/static/img/logo_square.png"
+#     }
+#   }
+# }
+  end
+end
