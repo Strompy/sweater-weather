87d718ccef4ba9f4968346db7e6766aaa379b543
Phillip Strom <phillip.strom@gmail.com>
2020-09-20 13:53:11 -0600

    Add vcr to test

--- spec/requests/api/v1/forecasts/retrieve_weather_by_city_spec.rb
+++ spec/requests/api/v1/forecasts/retrieve_weather_by_city_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Get Weather by City Endpoint' do
-  it "can receive a location and return the weather data" do
+  it "can receive a location and return the weather data", :vcr do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
