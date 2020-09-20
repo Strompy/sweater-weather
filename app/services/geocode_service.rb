7fe062fe649497ae0ae7ec97017d0a3865241fce
Phillip Strom <phillip.strom@gmail.com>
2020-09-19 19:00:59 -0600

    Create geocode service

---
+++ app/services/geocode_service.rb
@@ -0,0 +1,22 @@
+class GeocodeService
+  def self.search(location)
+    response = get_json(location)
+    parse = JSON.parse(response.body, symbolize_names: true)
+    parse[:results][0][:locations][0][:latLng]
+    require "pry"; binding.pry
+  end
+
+  private
+
+  def self.get_json(location)
+    response = conn.get('address') do |req|
+      req.params['location']= location
+    end
+  end
+
+  def self.conn
+    conn = Faraday.new(ENV['GEOCODE_SERVICE_URL']) do |f|
+      f.params[:key] = ENV['MAPQUEST_CONSUMER_KEY']
+    end
+  end
+end
