5aeea727939a108623344f4598f5aa3a8e31360d
Phillip Strom <phillip.strom@gmail.com>
2020-09-20 16:00:34 -0600

    Change variable and remove pry

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    forecast_object = ForecastFacade.forecast_by_location(params[:location])
-    require "pry"; binding.pry
-    render json: ForecastSerializer.render(forecast)
+    forecast = ForecastFacade.forecast_by_location(params[:location])
+    render json: ForecastSerializer.new(forecast).serialized_json
   end
 end
