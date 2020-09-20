01ed9f8f4f2a0c6cc63186230298cece990f3257
Phillip Strom <phillip.strom@gmail.com>
2020-09-20 13:53:02 -0600

    Add filter for weather api data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,6 +73,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('<MAPQUEST_API_KEY>') { ENV['MAPQUEST_CONSUMER_KEY'] }
+  config.filter_sensitive_data('<OPENWEATHER_API_KEY>') { ENV['OPENWEATHER_API_KEY'] }
   config.default_cassette_options = { re_record_interval: 2.days }
   config.configure_rspec_metadata!
 end
