b19ea146e60987ea980e42ee22b582d52e0bac5b
Phillip Strom <phillip.strom@gmail.com>
2020-09-21 11:38:59 -0600

    Refactor forecast methods

--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -1,16 +1,20 @@
 class ClimbingRoutesFacade
   def self.route_info(location)
     geocode = GeocodeService.search(location)
-    forecast_data = ForecastService.forecast_by_coordinates(geocode)
-    forecast = Forecast.new(forecast_data, location)
-    routes_data = RoutesService.nearby_routes(geocode)
-    routes = create_routes(routes_data, geocode)
+    forecast = self.get_forecast(geocode, location)
+    routes = create_routes(geocode)
     ClimbingRoute.new(location, forecast, routes)
   end
 
   private
 
-  def self.create_routes(routes, starting_coordinates)
+  def self.get_forecast(geocode, location)
+    forecast_data = ForecastService.forecast_by_coordinates(geocode)
+    Forecast.new(forecast_data, location)
+  end
+
+  def self.create_routes(starting_coordinates)
+    routes = RoutesService.nearby_routes(starting_coordinates)
     from = starting_coordinates[:lat].to_s + "," + starting_coordinates[:lng].to_s
     routes.map do |route|
       to = route[:latitude].to_s + "," + route[:longitude].to_s
@@ -23,7 +27,6 @@ class ClimbingRoutesFacade
         "distance_to_route": distance
       }
     end
-
   end
 
 end
