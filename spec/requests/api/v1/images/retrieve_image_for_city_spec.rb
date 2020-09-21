6ad6389e1b003dc7d6e7560d599bf1153e50892e
Phillip Strom <phillip.strom@gmail.com>
2020-09-20 19:03:07 -0600

    Remove comments

--- spec/requests/api/v1/images/retrieve_image_for_city_spec.rb
+++ spec/requests/api/v1/images/retrieve_image_for_city_spec.rb
@@ -18,20 +18,5 @@ RSpec.describe "Get background image endpoint" do
     expect(image_info[:image_url]).to_not be_nil
     expect(image_info[:credit].keys).to eq([:source, :author, :logo])
     expect(image_info[:credit].values).to_not include(nil)
-
-
-# {
-#   "type": "image",
-#   "id": null,
-#   "image": {
-#     "location": "denver,co",
-#     "image_url": "https://pixabay.com/get/54e6d4444f50a814f1dc8460962930761c38d6ed534c704c7c2878dd954dc451_640.jpg",
-#     "credit": {
-#       "source": "pixabay.com",
-#       "author": "quinntheislander",
-#       "logo": "https://pixabay.com/static/img/logo_square.png"
-#     }
-#   }
-# }
   end
 end
