5818b1090ad9ac338b9ba3ef133a90ddb3cc03ef
Phillip Strom <phillip.strom@gmail.com>
2020-09-21 11:30:12 -0600

    Remove comments

--- spec/requests/api/v1/climbing_routes/retrieve_routes_spec.rb
+++ spec/requests/api/v1/climbing_routes/retrieve_routes_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
-# requirements
-# Your API will return:
-# current forecast for the start location
-# name of the route
-# type of each route
-# rating of each route
-# location of each route
-# estimated travel time to each route
 
 RSpec.describe "Climbing Routes Location endpoint" do
   it 'Get forecast for a location and nearby climbing routes with the distance to each trail', :vcr do
@@ -32,43 +24,3 @@ RSpec.describe "Climbing Routes Location endpoint" do
     end
   end
 end
-# {
-#   "data": {
-#     "id": "null",
-#     "type": "climbing route",
-#     "attributes": {
-#       "location": "erwin,tn",
-#       "forecast": {
-#         "summary": "Raining cats and dogs",
-#         "temperature": "65"
-#       },
-#       "routes": [
-#         {
-#           "name": "Dopey Duck",
-#           "type": "Trad",
-#           "rating": "5.9",
-#           "location": [
-#             "North Carolina",
-#             "2. Northern Mountains Region",
-#             "Linville Gorge",
-#             "Shortoff Mountain"
-#           ]
-#           "distance_to_route": "76.876"
-#         },
-#         {
-#           "name": "Straight and Narrow",
-#           "type": "Trad"
-#           "rating": "5.10a"
-#           "location": [
-#             "North Carolina",
-#             "2. Northern Mountains Region",
-#             "Linville Gorge",
-#             "Shortoff Mountain"
-#           ]
-#           "distance_to_route": "76.876"
-#         },
-#         {...}
-#       ]
-#     }
-#   }
-# }
