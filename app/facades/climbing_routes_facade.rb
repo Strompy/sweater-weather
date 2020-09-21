6172b6d416ee5c9f7741ea18ad96aab298186e83
Phillip Strom <phillip.strom@gmail.com>
2020-09-21 11:08:41 -0600

    Builde create routes method

--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -1,12 +1,33 @@
 class ClimbingRoutesFacade
   def self.route_info(location)
-    require "pry"; binding.pry
     geocode = GeocodeService.search(location)
     forecast_data = ForecastService.forecast_by_coordinates(geocode)
-    foreForecast.new(forecast_data, location)
-    RoutesService.nearby_routes(geocode)
+    forecast = Forecast.new(forecast_data, location)
+    routes_data = RoutesService.nearby_routes(geocode)
+    require "pry"; binding.pry
+    routes = create_routes(routes_data, geocode)
+    ClimbingRoute.new(location, forecast, routes)
     # forecast_info needed => summary, temperature
+
     # DirectionsService.directions(location?)
+  end
+
+  private
+
+  def self.create_routes(routes, starting_coordinates)
+    from = starting_coordinates[:lat].to_s + "," + starting_coordinates[:lng].to_s
+    routes.map do |route|
+      to = route[:latitude].to_s + "," + route[:longitude].to_s
+      distance = DirectionsService.directions(from, to)[:distance]
+      {
+        "name": route[:name],
+        "type": route[:type],
+        "rating": route[:rating],
+        "location": route[:location],
+        "distance_to_route": distance
+      }
+    end
+
   end
 
 end
