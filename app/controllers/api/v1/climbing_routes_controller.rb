42ea485187647a1d3cea1a541fe71ba5961bf15d
Phillip Strom <phillip.strom@gmail.com>
2020-09-21 11:48:51 -0600

    Remove unecessary variable

--- app/controllers/api/v1/climbing_routes_controller.rb
+++ app/controllers/api/v1/climbing_routes_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::ClimbingRoutesController < ApplicationController
   def index
-    location = params[:location]
-    climbing_route = ClimbingRoutesFacade.route_info(location)
+    climbing_route = ClimbingRoutesFacade.route_info(params[:location])
     render json: ClimbingRouteSerializer.new(climbing_route).serialized_json
   end
 end
