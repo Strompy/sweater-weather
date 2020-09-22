a43ad0dd62716a447318317d507b66a28e117533
Phillip Strom <phillip.strom@gmail.com>
2020-09-21 20:01:11 -0600

    Create users controller and create action

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    require "pry"; binding.pry
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
