4ef27e4c3d79e02cd8f06682a25f5fc3a1d6bb88
Phillip Strom <phillip.strom@gmail.com>
2020-09-21 20:22:36 -0600

    Add sad path to create route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
     user = User.new(user_params)
     if user.save
       render json: UserSerializer.new(user), status: :created
+    else
+      errors = user.errors.full_messages.to_sentence
+      render json: { errors: errors }, status: 400
     end
   end
 
