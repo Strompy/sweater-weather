ec62b0f10f8b857501004ff6bc77f8f1ee0e882d
Phillip Strom <phillip.strom@gmail.com>
2020-09-22 21:13:41 -0600

    Add tests for data types

--- spec/requests/api/v1/forecasts/retrieve_weather_by_city_spec.rb
+++ spec/requests/api/v1/forecasts/retrieve_weather_by_city_spec.rb
@@ -18,8 +18,9 @@ RSpec.describe 'Get Weather by City Endpoint' do
 
     date_time = attributes[:date_time_info]
     expect(date_time.keys).to eq([:timezone, :offset])
-    expect(date_time[:timezone]).to_not eq(nil)
-    expect(date_time[:offset]).to_not eq(nil)
+
+    expect(date_time[:timezone].class).to eq(String)
+    expect(date_time[:offset].class).to eq(Integer)
 
     current = attributes[:current]
     expect(current.keys).to eq(
@@ -38,12 +39,26 @@ RSpec.describe 'Get Weather by City Endpoint' do
       ]
     )
     expect(current.values).to_not include (nil)
+    expect(current[:date_time].class).to eq(Integer)
+    expect(current[:temp].class).to eq(Float)
+    expect(current[:description].class).to eq(String)
+    expect(current[:high].class).to eq(Float)
+    expect(current[:low].class).to eq(Float)
+    expect(current[:sunrise].class).to eq(Integer)
+    expect(current[:sunset].class).to eq(Integer)
+    expect(current[:feels_like].class).to eq(Float)
+    expect(current[:humidity].class).to eq(Integer)
+    expect(current[:visibility_in_miles].class).to eq(Float)
+    expect(current[:uv_index].class).to eq(Float)
 
     hourly = attributes[:hourly]
     expect(hourly.size).to eq(8)
     hourly.each do |hour|
       expect(hour.keys).to eq([:date_time, :temp, :description])
       expect(hour.values).to_not include(nil)
+      expect(hour[:date_time].class).to eq(Integer)
+      expect(hour[:temp].class).to eq(Float)
+      expect(hour[:description].class).to eq(String)
     end
 
     daily = attributes[:daily]
@@ -59,6 +74,11 @@ RSpec.describe 'Get Weather by City Endpoint' do
           ]
         )
         expect(day.values).to_not include(nil)
+        expect(day[:date_time].class).to eq(Integer)
+        expect(day[:high_temp].class).to eq(Float)
+        expect(day[:low_temp].class).to eq(Float)
+        expect(day[:total_precipitation].class).to eq(Integer)
+        expect(day[:description].class).to eq(String)
     end
   end
 end
